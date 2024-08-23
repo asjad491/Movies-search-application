afd60110de2e8d511583b73eb4ff7a17d5b52adb
Asjad Afzaal <132608659+asjad491@users.noreply.github.com>
2024-08-23 05:28:10 -0400

    edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = ({ onMoviesClick, onTvShowsClick }) => {
           <button
             type="button"
             className="hidden md:block text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-2 me-2 mb-2"
-            onClick={() => window.open('https://github.com/asjad491', '_blank')}
+            onClick={() => window.open('https://github.com/asjad491/Movies-search-application', '_blank')}
           >
             Get Code!
           </button>
